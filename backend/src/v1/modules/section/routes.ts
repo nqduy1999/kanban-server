6b2004886f3234c5807c742859191cdfde89fe35
super-intern-dev <duy.se.it99@gmail.com>
2022-09-22 17:33:50 +0700

    feat(section): add section router

---
+++ backend/src/v1/modules/section/routes.ts
@@ -0,0 +1,54 @@
+import * as expressRouter from 'express'
+import { tokenHandler, validation } from '../guards'
+import { SectionController } from './controller'
+import { param } from 'express-validator'
+
+const router = expressRouter.Router()
+
+router.post(
+  '/',
+  param('boardId').custom(value => {
+    if (!validation.isObjectId(value)) {
+      return Promise.reject('invalid id')
+    } else return Promise.resolve()
+  }),
+  validation.validate,
+  tokenHandler.verifyToken,
+  SectionController.create
+)
+
+router.put(
+  '/:sectionId',
+  param('boardId').custom(value => {
+    if (!validation.isObjectId(value)) {
+      return Promise.reject('invalid board id')
+    } else return Promise.resolve()
+  }),
+  param('sectionId').custom(value => {
+    if (!validation.isObjectId(value)) {
+      return Promise.reject('invalid section id')
+    } else return Promise.resolve()
+  }),
+  validation.validate,
+  tokenHandler.verifyToken,
+  SectionController.update
+)
+
+router.delete(
+  '/:sectionId',
+  param('boardId').custom(value => {
+    if (!validation.isObjectId(value)) {
+      return Promise.reject('invalid board id')
+    } else return Promise.resolve()
+  }),
+  param('sectionId').custom(value => {
+    if (!validation.isObjectId(value)) {
+      return Promise.reject('invalid section id')
+    } else return Promise.resolve()
+  }),
+  validation.validate,
+  tokenHandler.verifyToken,
+  SectionController.delete
+)
+
+export { router as sectionRouter }
