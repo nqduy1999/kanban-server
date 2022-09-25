daf112063e0360779cfc36a12744d40ef9b9b8b6
Super Intern Dev <duy.se.it99@gmail.com>
2022-09-25 18:24:14 +0700

    chore: remove junks

--- frontend/src/components/molecules/form-input/form-input.molecule.tsx
+++ frontend/src/components/molecules/form-input/form-input.molecule.tsx
@@ -1,6 +1,6 @@
 import { IInput, Input } from "@/components/atoms";
 import { FieldMetaProps } from "formik";
-import { FC, useState } from "react";
+import { FC } from "react";
 
 export interface IFormInput extends IInput {
   meta: FieldMetaProps<any>;
@@ -18,8 +18,6 @@ const FormInput: FC<IFormInput> = ({
   wrapperClassName,
   ...rest
 }) => {
-  const [showPassword, setShowPassword] = useState<boolean>(false)
-  
   return (
     <div className={`w-full flex flex-col ${wrapperClassName ?? ""}`}>
       {label && (
@@ -27,8 +25,9 @@ const FormInput: FC<IFormInput> = ({
           {label}
         </label>
       )}
-      <Input id={id} {...rest} />
-      {type === 'password' && <></> }
+      <div className="relative w-full">
+        <Input id={id} {...rest} type={type} />
+      </div>
       {meta.touched && meta.error && (
         <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
           {meta.error}
@@ -42,6 +41,6 @@ FormInput.defaultProps = {
   classLabel:
     "font-semibold text-sm leading-none text-gray-900 dark:text-white ",
   className:
-    "leading-none text-gray-900 dark:text-gray-50 p-3 focus:outline-none border-gray-400	 dark:border-gray-600 mt-2 border-2 dark:bg-gray-600 rounded bg-white",
+    "leading-none w-full text-gray-900 dark:text-gray-50 p-3 focus:outline-none border-gray-400 dark:border-gray-600 mt-2 border-2 dark:bg-gray-600 rounded bg-white",
 };
 export { FormInput };
