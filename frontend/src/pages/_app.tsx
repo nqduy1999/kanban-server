644eb831b94aa6cfada25499f33a79caa92b0a5f
super-intern-dev <duy.se.it99@gmail.com>
2022-09-30 21:59:14 +0700

    chore: add react-query

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import "../styles/global.css";
 import { ThemeProvider } from "next-themes";
-
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import type { AppProps } from "next/app";
+import React from "react";
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ThemeProvider enableSystem={true} attribute="class">
-    <Component {...pageProps} />
-  </ThemeProvider>
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [queryClient] = React.useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider enableSystem={true} attribute="class">
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default MyApp;
