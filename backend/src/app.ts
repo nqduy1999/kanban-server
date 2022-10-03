0810049663ed397a8232a4fad79023a15090365f
Super Intern Dev <duy.se.it99@gmail.com>
2022-10-03 23:45:10 +0700

    chore: backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,33 +1,44 @@
-import * as express from "express";
-import * as bodyParser from "body-parser";
 import * as logger from "morgan";
 import * as cors from "cors";
 import * as cookieParser from "cookie-parser";
-import * as path from 'path';
+import * as path from "path";
 import * as dotenv from "dotenv";
+import {
+  authRouter,
+  boardRouter,
+  sectionRouter,
+  taskRouter,
+  adminRouter,
+} from "./v1/routes";
+import * as express from "express";
+import * as bodyParser from "body-parser";
 
-import { authRouter, boardRouter, sectionRouter, taskRouter, adminRouter } from "./v1/routes";
+class App {
+  public app: express.Application;
 
+  constructor() {
+    this.app = express();
+    this.config();
+    dotenv.config();
+  }
 
-const app: express.Express = express()
-dotenv.config()
-
-function config(): void {
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(cors())
-  app.use(logger('dev'));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use('/api/v1/auth', authRouter)
-  app.use('/api/v1/board', boardRouter)
-  app.use('/api/v1/section', sectionRouter)
-  app.use('/api/v1/task', taskRouter)
-  app.use('/api/v1/admin', adminRouter)
-
+  private config(): void {
+    // Giúp chúng ta tiếp nhận dữ liệu từ body của request
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+    this.app.use(cors());
+    this.app.use(logger("dev"));
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(cookieParser());
+    this.app.use(express.static(path.join(__dirname, "public")));
+    this.app.use("/api/v1/auth", authRouter);
+    this.app.use("/api/v1/board", boardRouter);
+    this.app.use("/api/v1/section", sectionRouter);
+    this.app.use("/api/v1/task", taskRouter);
+    this.app.use("/api/v1/admin", adminRouter);
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+  }
 }
-config()
 
-export default app;
+export default new App().app;
