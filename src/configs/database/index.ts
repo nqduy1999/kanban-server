3d9d7d806e8ac32179eba9d5e929c83e47eba0e9
super intern dev <duy.se.it99@gmail.com>
2022-10-04 11:40:12 +0700

    chore: update script

--- src/configs/database/index.ts
+++ src/configs/database/index.ts
@@ -1,13 +1,11 @@
 import mongoose, { ConnectOptions } from "mongoose"
 
-console.log(process.env.ISLOCAL, 'IS LOCAL');
-
-const URI = (process.env.ISLOCAL !== "false" ?  "mongodb://127.0.0.1:27017/kanban" : process.env.MONGODB_URL)
+const URI = (process.env.ISLOCAL !== "false" ? "mongodb://127.0.0.1:27017/kanban" : process.env.MONGODB_URL)
 
 mongoose.connect(`${URI}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-} as ConnectOptions).then(() => {  
+} as ConnectOptions).then(() => {
   console.log("Database Connected Successfuly.");
 })
   .catch((err) => {
